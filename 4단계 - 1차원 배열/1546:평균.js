3e5c82f7b97fc731da18d4bdab06f66d4ba720a4
Hansong Kim <101563532+hansong0222@users.noreply.github.com>
2022-08-22 21:20:59 +0900

    220822

--- 4단계 - 1차원 배열/1546:평균.js
+++ 4단계 - 1차원 배열/1546:평균.js
@@ -14,7 +14,7 @@
 */
 
 const fs = require("fs");
-let input = fs.readFileSync("./input.txt").toString().trim().split("\n");
+let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
 let n = +input[0];
 let numArr = input[1].split(" ").map(x=>Number(x));
