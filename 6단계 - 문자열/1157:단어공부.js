741607239262f6f6b59b067851de1cf126c7fba8
Hansong Kim <101563532+hansong0222@users.noreply.github.com>
2022-08-26 23:35:40 +0900

    220826

--- 6단계 - 문자열/1157:단어공부.js
+++ 6단계 - 문자열/1157:단어공부.js
@@ -9,7 +9,7 @@
 첫째 줄에 이 단어에서 가장 많이 사용된 알파벳을 대문자로 출력한다. 단, 가장 많이 사용된 알파벳이 여러 개 존재하는 경우에는 ?를 출력한다.
 */
 const fs = require("fs");
-let input = fs.readFileSync("./input.txt").toString().trim().toLowerCase();
+let input = fs.readFileSync("/dev/stdin").toString().trim().toLowerCase();
 
 let arr = new Array(26).fill(0);
 
